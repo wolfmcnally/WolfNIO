eecc7c52155c2fb428e4d94134fe9ee42afceb32
Wolf McNally <wolf@wolfmcnally.com>
2019-09-27 16:20:30 -0700

    Fixed dependencies.

--- Package.swift
+++ Package.swift
@@ -11,9 +11,13 @@ let package = Package(
             name: "WolfNIO",
             targets: ["WolfNIO"]),
         ],
+    dependencies: [
+        .package(url: "https://github.com/apple/swift-nio.git", from: "2.2.0"),
+        .package(url: "https://github.com/apple/swift-nio-transport-services.git", from: "1.0.0")
+    ],
     targets: [
         .target(
             name: "WolfNIO",
-            dependencies: [])
+            dependencies: ["NIO", "NIOTransportServices"])
         ]
 )
